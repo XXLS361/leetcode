da6cb1a3e604e7d3af94c039d49ac3e7a1af840c
XXLS361 <xxls361@163.com>
2021-11-25 10:10:05 +0800

    push

---
+++ 23.合并k个升序链表.cpp
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=23 lang=cpp
+ *
+ * [23] 合并K个升序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        auto head = ListNode(0);
+        auto comp = [](ListNode* const &a,ListNode* const &b){return a->val > b->val;};
+        priority_queue<ListNode*,vector<ListNode*>,decltype(comp)> q(comp);
+        for(auto &h:lists){
+            if(h!=nullptr){
+                q.push(h);
+            }
+        }
+        auto p = &head;
+        while(!q.empty()){
+            p->next = q.top();
+            p=p->next;
+            q.pop();
+            if(p->next!=nullptr){
+                q.push(p->next);
+            }
+        }
+        return head.next;
+    }
+};
+// @lc code=end
+
